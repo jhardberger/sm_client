ec823d44430417f588d2f602cf9a8c2db5fe407e
John <john.hardberger@gmail.com>
2018-12-03 23:07:07 -0600

    set up hash params in react to get token from the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    console.log(params);
+  }
+
+  getHashParams(){
+
+    //gets access token from params (expanded from index.html code on backend)
+    let hashParams = {};
+    let e = /([^&;=]+)=?([^&;]*)/g;
+    const r = /([^&;=]+)=?([^&;]*)/g; 
+    const q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+
+    //returns access token
+    console.log(hashParams);
+    return hashParams;
+  }
+
   render() {
     return (
-      <div>
-        <p>hi!</p>
-      </div>
+    <div className='App'>
+      <a href='http://localhost:8888'> Login to Spotify </a>
+    </div>
     );
   }
 }
